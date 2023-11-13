03cc6d38cb9cb901cf5a3581aa91f40d9af7dac9
N-Dinot <103276507+N-Dinot@users.noreply.github.com>
2023-11-13 15:59:39 +0100

    feat : CRUD product

---
+++ src/main/java/fr/negosud/springapi/api/controller/ProductController.java
@@ -0,0 +1,70 @@
+package fr.negosud.springapi.api.controller;
+
+import fr.negosud.springapi.api.entity.Product;
+import fr.negosud.springapi.api.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+    private final ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProducts() {
+
+        List<Product> products = productService.getAllProducts();
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
+    @GetMapping("/{productId}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long productId) {
+
+        return productService.getProductById(productId)
+                .map(product -> new ResponseEntity<>(product, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+
+        Product createdProduct= productService.saveProduct(product);
+        return new ResponseEntity<>(createdProduct, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{productId}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Long productId, @RequestBody Product product) {
+
+        if (productService.getProductById(productId).isPresent()) {
+            product.setProductId(productId);
+            Product updatedProduct = productService.saveProduct(product);
+            return new ResponseEntity<>(updatedProduct, HttpStatus.OK);
+
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{productId}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long productId) {
+
+        if (productService.getProductById(productId).isPresent()) {
+            productService.deleteProduct(productId);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+}
---
+++ src/main/java/fr/negosud/springapi/api/service/ProductService.java
@@ -0,0 +1,41 @@
+package fr.negosud.springapi.api.service;
+
+import fr.negosud.springapi.api.entity.Product;
+import fr.negosud.springapi.api.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getAllProducts() {
+
+        return productRepository.findAll();
+    }
+
+    public Optional<Product> getProductById(Long productId) {
+
+        return productRepository.findById(productId);
+    }
+
+    public Product saveProduct(Product product) {
+
+        return productRepository.save(product);
+    }
+
+    public void deleteProduct(Long productId) {
+
+        productRepository.deleteById(productId);
+    }
+}
